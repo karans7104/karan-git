6e6b162cc9bee37b053f00b0a8113b54ef52c150
karans7104 <145362457+karans7104@users.noreply.github.com>
2025-06-06 01:35:24 +0000

    conversion and datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,33 @@
+// primitive
+
+//7types: string,numberr,boolean,null,undefined,symbol,BigInt
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber =2826526896162527682n
+
+//Reference (NOn Primitive)
+
+//array, objects, functions
+
+const heros= ["shaktiman","naagraj", "doga"];
+let myObj = {
+    name :"vinu",
+    age: 22,
+}
+
+const myFunction =function()
+{
+    console.log("Hello world");
+}
+
+console.log(typeof null);
